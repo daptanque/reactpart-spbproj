946a19a2e7ec36c7cf5528d294f50b6d57ff06f3
daptanque <daniel.apeixoto.tanque@gmail.com>
2023-04-06 19:43:28 +0100

    final changes - proj complete

--- spb-react/src/App.js
+++ spb-react/src/App.js
@@ -1,11 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import CreateEmployeeComponent from './components/CreateEmployeeComponent';
+import UpdateEmployeeComponent from './components/UpdateEmployeeComponent';
+import ViewEmployeeComponent from './components/ViewEmployeeComponent';
 
 function App() {
   return (
-    <div className="container">
-      <ListEmployeeComponent />
+    <div>
+      <Router>
+
+          <HeaderComponent />
+
+          <div className="container">
+            <Switch>
+              <Route path="/" exact component={ListEmployeeComponent} />
+              <Route path="/employees" component={ListEmployeeComponent} />
+              <Route path="/add-employee" component={CreateEmployeeComponent} />  
+              <Route path="/update-employee/:id" component={UpdateEmployeeComponent} />
+              <Route path="/view-employee/:id" component={ViewEmployeeComponent} />
+            </Switch>
+          </div>
+
+          <FooterComponent />
+        
+      </Router>
     </div>
   );
 }
--- spb-react/src/components/ListEmployeeComponent.jsx
+++ spb-react/src/components/ListEmployeeComponent.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import EmployeeService from '../services/EmployeeService';
+import { withRouter } from 'react-router-dom';
+
 
 class ListEmployeeComponent extends Component {
     constructor(props){
@@ -8,6 +10,10 @@ class ListEmployeeComponent extends Component {
         this.state = {
             employees: []
         }
+
+        this.addEmployee = this.addEmployee.bind(this);
+        this.editEmployee = this.editEmployee.bind(this);
+        this.deleteEmployee = this.deleteEmployee.bind(this);
     }
 
     componentDidMount(){
@@ -16,11 +22,35 @@ class ListEmployeeComponent extends Component {
         });
     }
 
+    addEmployee(){
+        this.props.history.push("/add-employee");
+        document.location.reload();
+    }
+
+    editEmployee(id){
+        this.props.history.push(`/update-employee/${id}`);
+        document.location.reload();
+    }
+
+    deleteEmployee(employeeId){
+        EmployeeService.deleteEmployee(employeeId).then(res => {
+            this.setState({employees: this.state.employees.filter(employee => employee.id !== employeeId)});
+        });
+    }
+
+    viewEmployee(id){
+        this.props.history.push(`/view-employee/${id}`);
+        document.location.reload();
+    }
+
+
     render() {
         return (
             <div>
                 <h2 className="text-center">Employees List</h2>
-
+                <div className='row'>
+                    <button className='bt btn-primary' onClick={this.addEmployee}>Add Employee</button>
+                </div>
                 <div className="row">
                     <table className="table table-striped table-bordered">
                         <thead>
@@ -40,7 +70,11 @@ class ListEmployeeComponent extends Component {
                                         <td> {employee.firstName}</td>
                                         <td> {employee.lastName}</td>
                                         <td> {employee.emailId}</td>
-                                        
+                                        <td>
+                                        <button onClick={() => this.editEmployee(employee.id)} className='btn btn-info'>Update</button>
+                                        <button style={{marginLeft: "10px"}} onClick={() => this.deleteEmployee(employee.id)} className='btn btn-danger'>Delete</button>
+                                        <button style={{marginLeft: "10px"}} onClick={() => this.viewEmployee(employee.id)} className='btn btn-success'>View</button>
+                                        </td>
                                      </tr>
                                 )
                             }
@@ -53,4 +87,4 @@ class ListEmployeeComponent extends Component {
     }
 }
 
-export default ListEmployeeComponent;
+export default withRouter (ListEmployeeComponent);
